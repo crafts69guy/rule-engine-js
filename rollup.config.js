ad278c39c6ac8bd13df9c9c16ec00eacbbb4d1e2
NgocCuong <cuongcn@antsomi.com>
2025-08-08 10:37:27 +0700

    chore: rollup config

---
+++ rollup.config.js
@@ -0,0 +1,60 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+
+const config = {
+  input: 'src/index.js',
+  external: [],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env']
+    })
+  ]
+};
+
+export default [
+  // UMD build (browser)
+  {
+    ...config,
+    output: {
+      file: 'dist/index.js',
+      format: 'umd',
+      name: 'RuleEngineJS',
+      sourcemap: true
+    }
+  },
+  // UMD minified
+  {
+    ...config,
+    output: {
+      file: 'dist/index.min.js',
+      format: 'umd',
+      name: 'RuleEngineJS',
+      sourcemap: true
+    },
+    plugins: [...config.plugins, terser()]
+  },
+  // ESM build
+  {
+    ...config,
+    output: {
+      file: 'dist/index.esm.js',
+      format: 'es',
+      sourcemap: true
+    }
+  },
+  // CommonJS build
+  {
+    ...config,
+    output: {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true
+    }
+  }
+];
